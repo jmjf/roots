78ad011400ece3d510835270fd8f412d665cac50
Jamee Mikell <jamee.mikell@gmail.com>
2013-09-26 08:35:09 -0400

    Use bloginfo to set alt on logo/banner images

--- templates/jmjf-header-image-nav-responsive-branded.php
+++ templates/jmjf-header-image-nav-responsive-branded.php
@@ -1,7 +1,9 @@
 <header class="banner container" role="banner">
 	<div class="row">
-		<div class="col-lg-12">
-			<a class "hidden-xs" href="<?php echo home_url(); ?>/"><img src="assets/img/headder-banner.png"></a>
+		<div class="col-sm-12">
+			<a class "hidden-xs" href="<?php echo home_url(); ?>/">
+				<img src="/assets/img/header-banner.png" alt="<?php bloginfo('name'); ?>" />
+			</a>
 			<div class="container navbar navbar-default">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -10,7 +12,9 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand visible-xs"> href="<?php echo home_url(); ?>/"><img src="assets/img/navbar-banner.png</a>
+					<a class="navbar-brand visible-xs" href="<?php echo home_url(); ?>/">
+						<img src="assets/img/navbar-banner.png" alt="<?php bloginfo('name'); ?>" />
+					</a>
 				</div>
 
 				<nav class="collapse navbar-collapse" role="navigation">
